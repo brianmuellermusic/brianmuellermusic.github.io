60ed99532304dacb58ce4cadff0f6cd69a70ecad
brianmuellermusic <brianmuellermusic@gmail.com>
2021-10-01 12:53:41 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let ownerName = "Your Name"; // update the part BETWEEN the "quotes"
-let userName = "YourUsername"; // same here
+let ownerName = "B. Mueller"; // update the part BETWEEN the "quotes"
+let userName = "brianmuellermusic"; // same here
 
 document.querySelectorAll(".owner-name").forEach((e) => {
     e.innerHTML = ownerName;
